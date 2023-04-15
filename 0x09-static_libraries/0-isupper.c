c97577670581467bed2f7c6517612a67172679e7
ghost-254 <nevilleotieno6@gmail.com>
2023-04-15 00:55:11 -0700

    Happy to make these changes

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,14 +1,14 @@
 #include "main.h"
+
 /**
- * _isupper - uppercase letters
- * @c: char to check
+ * _isupper - checks if a character is uppercase
+ * @c: character to check
  *
- * Return: 0 or 1
+ * Return: 1 if c is uppercase, 0 otherwise
  */
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 		return (1);
-	else
 	return (0);
 }
--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,31 +1,27 @@
 #include "main.h"
- /**
- * _strcat - concatenates two strings
- * @dest: input value
- * @src: input value
- *
- * Return: void	
+
+/**
+ * _strcat - concatenate two strings
+ * @dest: string to be appended to
+ * @src: string to append
+ * Return: concatenated string
  */
+
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+	int i = 0;
+	int j = 0;
 
-
-	i = 0;
 	while (dest[i] != '\0')
-	{
 		i++;
-	}
-	j = 0;
+
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
 		i++;
 		j++;
-	}		
-
-
+	}
 	dest[i] = '\0';
+
 	return (dest);
 }
