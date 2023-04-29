d26379173aca3588ca44be5e324a3a7596f099b3
Ghost <125441875+ghost-254@users.noreply.github.com>
2023-04-29 16:40:33 +0300

    Update 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -2,28 +2,15 @@
 ; Description: Working on writing a 64-bit program..
 ; ...in assembly that prints Hello, Holberton
 
-global _start
-
 section .data
-	msg db "Hello, Holberton", 0 ; Null-terminated string
-	fmt db "%s", 10, 0 ; Format string with newline and null terminator
+	message db "Hello, Holberton", 0Ah, 0
 
 section .text
-_start:
-	; Push stack frame
-	push rbp
-	mov rbp, rsp
+	global main
+	extern printf
 
-	; Call printf
-	mov edi, fmt ; First argument: format string
-	mov esi, msg ; Second argument: message string
-	xor eax, eax ; Clear return value register
+main:
+	push message
 	call printf
-
-	; Clean up stack frame
-	mov rsp, rbp
-	pop rbp
-
-	; Exit program
-	mov eax, 0
+	add rsp, 8
 	ret
