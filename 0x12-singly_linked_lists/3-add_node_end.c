74a317524894961f633bc0096557914de3b32c29
Ghost <125441875+ghost-254@users.noreply.github.com>
2023-04-29 14:01:15 +0300

    Update 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -14,41 +14,37 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	if (head == NULL) {
-        return NULL;
-    }
+	if (head == NULL)
+		return NULL;
 
-    if (str == NULL) {
-        return NULL;
-    }
+	if (str == NULL)
+		return NULL;
 
-    list_t *new = malloc(sizeof(list_t));
-    if (new == NULL) {
-        return NULL;
-    }
+	list_t *new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return NULL;
 
-    new->str = strdup(str);
-    if (new->str == NULL) {
-        free(new);
-        return NULL;
-    }
+	new->str = strdup(str);
+	if (new->str == NULL)
+		free(new);
+	return NULL;
 
-    int len = 0;
-    while (str[len] != '\0') {
-        len++;
-    }
-    new->len = len;
-    new->next = NULL;
+	int len = 0;
+	while (str[len] != '\0')
+		len++;
 
-    if (*head == NULL) {
-        *head = new;
-    } else {
-        list_t *last = *head;
-        while (last->next != NULL) {
-            last = last->next;
-        }
-        last->next = new;
-    }
+	new->len = len;
+	new->next = NULL;
 
-    return *head;
+	if (*head == NULL)
+	*head = new;
+	else {
+		list_t *last = *head;
+		while (last->next != NULL)
+			last = last->next;
+
+		last->next = new;
+	}
+
+		return *head;
 }
